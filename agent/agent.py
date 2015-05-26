1eac745597875ec5abe32b1b92f9e419755d8958
Mateusz Kurek <master.mateusz@gmail.com>
2015-05-26 21:10:25 +0200

    set network arch through env variable

--- agent/agent.py
+++ agent/agent.py
@@ -14,6 +14,8 @@ class EnvDefault(argparse.Action):
     def __init__(self, envvar, required=False, default=None, **kwargs):
         if envvar and envvar in os.environ:
             default = os.environ[envvar]
+            if kwargs.get('nargs') in ('*', '+'):
+                default = map(lambda x: kwargs['type'](x.strip()), default.split(','))
         if required and default:
             required = False
         super(EnvDefault, self).__init__(default=default, required=required,
