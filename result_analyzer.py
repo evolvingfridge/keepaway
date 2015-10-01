63cad7fa8b4ed0dbc49b1a81447bda6b217018ee
Mateusz Kurek <mateusz.kurek@allegrogroup.com>
2015-10-01 20:35:31 +0200

    evaluation boxplot

--- result_analyzer.py
+++ result_analyzer.py
@@ -64,10 +64,34 @@ def get_evaluation_params():
 
 
 def get_agent_env():
-    agent_env_path = os.path.join(args.logs_directory, 'agent.env')
+    agent_env_path = os.path.expanduser(os.path.join(args.logs_directory, 'agent.env'))
     if os.path.exists(agent_env_path):
         return '\n'.join(open(agent_env_path, 'r').readlines())
     return 'CONF NOT PROVIDED'
+
+
+# http://code.activestate.com/recipes/511478/
+def percentile(N, percent, key=lambda x: x):
+    """
+    Find the percentile of a list of values.
+
+    @parameter N - is a list of values. Note N MUST BE already sorted.
+    @parameter percent - a float value from 0.0 to 1.0.
+    @parameter key - optional key function to compute value from each element of N.
+
+    @return - the percentile of the values
+    """
+    if not N:
+        return None
+    N = sorted(N)
+    k = (len(N)-1) * percent
+    f = math.floor(k)
+    c = math.ceil(k)
+    if f == c:
+        return key(N[int(k)])
+    d0 = key(N[int(f)]) * (c-k)
+    d1 = key(N[int(c)]) * (k-f)
+    return d0 + d1
 
 
 def save_graph(additional_opts, series):
@@ -91,6 +115,25 @@ def save_graph(additional_opts, series):
             # import ipdb; ipdb.set_trace()
 
 
+def save_eval_graph(additional_opts):
+    options = {
+        'max_x': "10000",
+        'max_y': "30",
+        'title': 'Graph',
+        'y_title': 'Episode Duration (seconds)',
+        'terminal': 'svg',
+    }
+    window_opts = options.copy()
+    window_opts.update(additional_opts)
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tools', 'graph_eval.gnuplot.tmpl')) as graph_tmpl:
+        with tempfile.NamedTemporaryFile('w') as f_window_graph:
+            g = graph_tmpl.read().format(**window_opts)
+            f_window_graph.write(g)
+            f_window_graph.flush()
+            print(f_window_graph.name)
+            subprocess.call(['gnuplot', f_window_graph.name])
+
+
 def save_histogram(additional_opts):
     options = {
         'max': int(additional_opts['max_episode_length']),
@@ -110,7 +153,7 @@ def save_histogram(additional_opts):
 
 
 # def process_kwy(f_evaluation_std, f_evaluation_confidence, f_window, f_window_episodes, f_window_mean, f_window_episodes_mean):
-def process_kwy(f_window, f_window_episodes, f_stats, f_histogram, f_evaluation_std, f_evaluation_confidence):
+def process_kwy(f_window, f_window_episodes, f_stats, f_histogram, f_evaluation_raw, f_evaluation_stats):
     evaluation_each, evaluation_length = get_evaluation_params()
     out_files = (f_window, f_window_episodes)
     constants_files = (f_window, f_window_episodes)
@@ -139,6 +182,7 @@ def process_kwy(f_window, f_window_episodes, f_stats, f_histogram, f_evaluation_
         hours_diff = 0.0
         evaluation_episodes = []
         evaluations = 0
+        eval_mean = 0
         print('Processing {}'.format(f_full))
         with open(f_full) as f_obj:
             for line in f_obj.readlines():
@@ -183,7 +227,7 @@ def process_kwy(f_window, f_window_episodes, f_stats, f_histogram, f_evaluation_
                             eval_mean = statistics.mean(evaluation_episodes)
                             # stdev = statistics.stdev(evaluation_episodes, xbar=eval_mean)
                             # confidence = 1.96 * stdev / math.sqrt(len(evaluation_episodes))
-                            # f_evaluation_std.write(' '.join(map(str, (
+                            # f_evaluation_raw.write(' '.join(map(str, (
                             #     episodes_count,
                             #     eval_mean,
                             #     stdev,
@@ -225,27 +269,63 @@ def process_kwy(f_window, f_window_episodes, f_stats, f_histogram, f_evaluation_
                 f_histogram.write(str(val) + '\n')
                 max_episode_length = max(max_episode_length, int(val))
 
-    f_evaluation_std.write('Deep-Q-Learning\n')
-    f_evaluation_confidence.write('Deep-Q-Learning\n')
-    for c, values in evaluation_stats.items():
+    # f_evaluation_raw.write('Deep-Q-Learning\n')
+    # f_evaluation_stats.write('Deep-Q-Learning\n')
+    f_evaluation_stats.write('\t'.join((
+        '# x',
+        'min',
+        '1_quart',
+        'avg',
+        'median',
+        'conf',
+        'stdev',
+        '3_quart',
+        'max',
+        '\n'
+    )))
+    max_eval = 0
+    for i, (c, values) in enumerate(sorted(evaluation_stats.items(), key=lambda x: x[0])):
         eval_mean = statistics.mean(values)
+        eval_median = statistics.median(values)
         stdev = statistics.stdev(values, xbar=eval_mean) if len(values) > 1 else 0
         confidence = 1.96 * stdev / math.sqrt(len(values))
-        f_evaluation_std.write(' '.join(map(str, (
-            c,
-            eval_mean,
-            stdev,
-            '\n',
-        ))))
-        f_evaluation_confidence.write(' '.join(map(str, (
-            c,
-            eval_mean,
-            confidence,
+        first_quartile = percentile(values, 0.25)
+        third_quartile = percentile(values, 0.75)
+        # f_evaluation_raw.write(' '.join(map(str, (
+        #     c,
+        #     eval_mean,
+        #     stdev,
+        #     '\n',
+        # ))))
+        max_eval = c - (i+2) * evaluation_length
+        f_evaluation_stats.write('\t'.join(map(str, (
+            max_eval,                        # 1
+            round(min(values), 2),    # 2
+            round(first_quartile, 2), # 3
+            round(eval_mean, 2),      # 4
+            round(eval_median, 2),    # 5
+            round(confidence, 2),     # 6
+            round(stdev, 2),          # 7
+            round(third_quartile, 2), # 8
+            round(max(values), 2),    # 9
             '\n',
         ))))
 
-    f_evaluation_std.write('\n\n')
-    f_evaluation_confidence.write('\n\n')
+        for val in values:
+            f_evaluation_raw.write(' '.join(map(str, (max_eval, val, '\n'))))
+
+    # boxplot
+    # f_evaluation_raw.write('# ')
+    # for key in sorted(evaluation_stats.keys()):
+    #     f_evaluation_raw.write('{} '.format(key))
+    # f_evaluation_raw.write('\n')
+    # for c in range(len(list(evaluation_stats.values())[0])):
+    #     for key in sorted(evaluation_stats.keys()):
+    #         f_evaluation_raw.write('{} '.format(evaluation_stats[key][c]))
+    #     f_evaluation_raw.write('\n')
+
+    f_evaluation_raw.write('\n\n')
+    f_evaluation_stats.write('\n\n')
 
     if args.draw_constants:
         for out_f in constants_files:
@@ -254,7 +334,7 @@ def process_kwy(f_window, f_window_episodes, f_stats, f_histogram, f_evaluation_
                 for tick in (0, (hours - hours_diff) if out_f in (f_window,) else episodes_count_without_eval):
                     out_f.write(' '.join(map(str, (tick, val, stdev, '\n'))))
                 out_f.write('\n\n')
-    for out_f in out_files + constants_files + (f_evaluation_std, f_evaluation_confidence):
+    for out_f in out_files + constants_files + (f_evaluation_raw, f_evaluation_stats):
         out_f.flush()
 
     series = i + (3 * args.draw_constants)
@@ -318,8 +398,8 @@ def process_kwy(f_window, f_window_episodes, f_stats, f_histogram, f_evaluation_
     # }, series)
 
     save_graph({
-        'cols': '1:2:3',
-        'file': f_evaluation_std.name,
+        'cols': '1:4:7',
+        'file': f_evaluation_stats.name,
         'out_file': os.path.join(args.logs_directory, 'window_graph_eval_std.{}'.format(PLOT_EXT)),
         'plot_options': 'w yerrorbars',
         'x_title': 'Episodes count',
@@ -327,8 +407,8 @@ def process_kwy(f_window, f_window_episodes, f_stats, f_histogram, f_evaluation_
     }, series)
 
     save_graph({
-        'cols': '1:2:3',
-        'file': f_evaluation_confidence.name,
+        'cols': '1:4:6',
+        'file': f_evaluation_stats.name,
         'out_file': os.path.join(args.logs_directory, 'window_graph_eval_conf.{}'.format(PLOT_EXT)),
         'plot_options': 'w yerrorbars',
         'x_title': 'Episodes count',
@@ -336,14 +416,25 @@ def process_kwy(f_window, f_window_episodes, f_stats, f_histogram, f_evaluation_
     }, series)
 
     save_graph({
-        'cols': '1:2',
-        'file': f_evaluation_std.name,
-        'out_file': os.path.join(args.logs_directory, 'window_graph_eval.{}'.format(PLOT_EXT)),
+        'cols': '1:5',
+        'file': f_evaluation_stats.name,
+        'out_file': os.path.join(args.logs_directory, 'window_graph_eval_median.{}'.format(PLOT_EXT)),
         # 'plot_options': 'w yerrorbars',
         'plot_options': 'w points',
         'x_title': 'Episodes count',
-        'title': 'Avg episode duration during evaluation ({} every {} episodes)'.format(evaluation_length, evaluation_each),
+        'title': 'Median episode duration during evaluation ({} every {} episodes)'.format(evaluation_length, evaluation_each),
     }, series)
+
+    save_eval_graph({
+        'file_stats': f_evaluation_stats.name,
+        'file_raw_data': f_evaluation_raw.name,
+        'point_offset': 0.01 * max(evaluation_stats.keys()),
+        'max_x': max_eval + 500,
+        'max_y': max([max(v) for v in evaluation_stats.values()]) * 1.2,
+        'out_file': os.path.join(args.logs_directory, 'window_graph_eval_box.{}'.format(PLOT_EXT)),
+        'x_title': 'Episodes count',
+        'title': 'Average episode duration during evaluation ({} every {} episodes)'.format(evaluation_length, evaluation_each-evaluation_length),
+    })
 
 
 def process_agent_logs(f_mean_q_delta, f_mean_q_steps, f_mean_starting_q):
@@ -485,10 +576,10 @@ def main():
                             process_kwy(f_window, f_window_episodes, f_stats, f_histogram, f_evaluation_std, f_evaluation_confidence)
 
     # process agent log files
-    with tempfile.NamedTemporaryFile('w') as f_mean_q_delta:
-        with tempfile.NamedTemporaryFile('w') as f_mean_q_steps:
-            with tempfile.NamedTemporaryFile('w') as f_mean_starting_q:
-                process_agent_logs(f_mean_q_delta, f_mean_q_steps, f_mean_starting_q)
+    # with tempfile.NamedTemporaryFile('w') as f_mean_q_delta:
+    #     with tempfile.NamedTemporaryFile('w') as f_mean_q_steps:
+    #         with tempfile.NamedTemporaryFile('w') as f_mean_starting_q:
+    #             process_agent_logs(f_mean_q_delta, f_mean_q_steps, f_mean_starting_q)
 
 if __name__ == '__main__':
     main()
